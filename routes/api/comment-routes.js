f74c928293d0a05be8b5531e1c85528792a42037
Luis Rosales <luismrosales2@gmail.com>
2021-12-01 13:36:02 -0500

    refactored and integreated front end

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const {
+  addComment,
+  removeComment,
+} = require("../../controllers/comment-controller");
+
+// api/comments/pizzaid
+router.route("/:pizzaId").post(addComment);
+
+// api/comments/pizzaid/commentid
+router.route("/:pizzaId/:commentId").delete(removeComment);
+
+module.exports = router;
